22d5ccfbeff9f55664388bc8ce9cb3c974386b6d
Youngjun Kim <youngjun.k97@gmail.com>
2024-11-01 10:51:30 +0900

    FEAT: Move password to Users's table

--- schedule.sql
+++ schedule.sql
@@ -2,12 +2,13 @@ CREATE TABLE Users (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
   username varchar(255) NOT NULL UNIQUE,
   email varchar(255) NOT NULL UNIQUE,
+  password varchar(255) NOT NULL,
   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Categories (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL UNIQUE,    
 );
 
@@ -19,8 +20,7 @@ CREATE TABLE Tasks (
   username varchar(255) NOT NULL,
   due_date DATE,
   priority ENUM('Low', 'Medium', 'High') DEFAULT 'Medium',
-  status ENUM('Pending', 'In Progress', 'Completed') DEFAULT 'Pending',
-  password varchar(255) NOT NULL,
+  status ENUM('Pending', 'In Progress', 'Completed') DEFAULT 'Pending',  
   created_at timestamp DEFAULT CURRENT_TIMESTAMP,
   updated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE,  
@@ -28,13 +28,13 @@ CREATE TABLE Tasks (
 );
 
 # Insert a new user
-INSERT INTO Users (username, email) VALUES ('youngjun', 'youngjun@gmil.com');
+INSERT INTO Users (username, email, password) VALUES ('youngjun', 'youngjun@gmil.com', 'password1234');
 
 # Insert a new category
 INSERT INTO Categories (name) VALUES ('Work');
 
 # Insert a new to-do list
-INSERT INTO tasks (content, user_id, category_id, username, status, password) VALUES('content', 1, 1, 'youngjun','2024-11-05', 'High', 'Pending', 'password1234' );
+INSERT INTO tasks (content, user_id, category_id, username, due_tate, status) VALUES('content', 1, 1, 'youngjun','2024-11-05', 'High', 'Pending' );
 
 # Select all to-do lists for a specific user
 SELECT * FROM tasks WHERE user_id = 1;
