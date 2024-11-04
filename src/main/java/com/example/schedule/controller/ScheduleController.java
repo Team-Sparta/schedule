8af237ebb44311e3521d25f391f4d1bd4f351dfc
Youngjun Kim <158126791+Youngjun97@users.noreply.github.com>
2024-11-04 17:53:28 +0900

    feat: initialization

---
+++ src/main/java/com/example/schedule/controller/ScheduleController.java
@@ -0,0 +1,13 @@
+package com.example.schedule.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/schedule")
+@AllArgsConstructor
+public class ScheduleController {
+
+
+}
